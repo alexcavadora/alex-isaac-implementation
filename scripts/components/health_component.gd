88149022a68f6e32c964a3fa439846579be81f2c
Alejandro Alonso Sánchez <alexaas110@gmail.com>
2025-06-28 15:11:19 -0600

    Enemy health bars and color indexing
    Implemented enemy health bars and color indexing to match the scheme proposed in the flag code.

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -4,22 +4,49 @@ class_name HealthComponent
 @onready var health_bar: ProgressBar = $HealthBar
 @export var max_hp : float 
 @export var hp : float
+@export_enum( "BLUE", "RED", "YELLOW") var color_idx = 0
+var color = 0
+"""
+- BLUE: 1
+- RED: 2
+- YELLOW: 3
+"""
+var fill_color : Color
 
 func apply_damage(incoming_dmg: float) -> void:
-	hp -= incoming_dmg
+	hp = max(0.0, hp - incoming_dmg)
 	if hp == 0.0:
 		die()
+	update_bar()
 
 func apply_heal(incoming_heal: float) -> void:
-	if max_hp <= (hp + incoming_heal):
-		hp += incoming_heal
-
+	hp = min(max_hp, hp + incoming_heal)
+	update_bar()
+	
 func die():
 	self.queue_free()
 
 func _ready() -> void:
 	health_bar.max_value = max_hp
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	health_bar.value = hp
+	color = color_idx + 1 #skip white index to start at blue (1)
+	print(color)
+	update_fill_color()
+
+func update_bar() -> void:
+	health_bar.value = hp
+
+func update_fill_color() -> void:
+	# select color from editor
+	match color:
+		1:  # BLUE
+			fill_color = Color.SKY_BLUE
+		2:  # RED
+			fill_color = Color.ORANGE_RED
+		3:  # YELLOW
+			fill_color = Color.YELLOW
+	
+	# apply the color to the health bar
+	var style_box = health_bar.get_theme_stylebox("fill").duplicate()
+	style_box.bg_color = fill_color
+	health_bar.add_theme_stylebox_override("fill", style_box)
