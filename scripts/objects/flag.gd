4d3452a8511ac7b08b5d0fcee1b2016d70cf3ffb
Alejandro Alonso Sánchez <49330757+alexcavadora@users.noreply.github.com>
2025-06-28 13:08:12 -0600

    Flag node succesfully added (#3)
    Simple but effective flag behaviour using an AnimatedSprite2D to change between the different states. Expects an array of 2 indices and updates accordingly

---
+++ scripts/objects/flag.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+@export var debug_previous: Array[int] = [0,0]
+
+var current_color = "white"
+
+"""
+white: 0, blue: 1, red: 2, yellow: 3
+"""
+const COLOR_COMBINATIONS = {
+	[1, 1]: "blue",
+	[2, 2]: "red",
+	[3, 3]: "yellow",
+	[1, 2]: "violet",
+	[2, 3]: "orange",
+	[1, 3]: "green"
+}
+
+#func _process(delta: float) -> void: #realtime debugging
+#	update_color(debug_previous)
+
+func _ready() -> void: #loading a saved state 
+	update_color(debug_previous)
+
+func update_color(previous_colors: Array[int]) -> String:
+	var color1 = previous_colors[0]
+	var color2 = previous_colors[1]
+
+	# empty state
+	if color1 == 0 and color2 == 0:
+		current_color = "white"
+		sprite_2d.play("white")
+		return current_color
+	
+	# first color with white also on queue
+	if color2 == 0:
+		color2 = color1
+
+	# sort pair to make mapping order-independent
+	var key = [color1, color2] if color1 < color2 else [color2, color1]
+	
+	# lookup result
+	current_color = COLOR_COMBINATIONS[key]
+	
+	# update sprite
+	sprite_2d.play(current_color)
+	
+	#if needed returns the color in string
+	return current_color
