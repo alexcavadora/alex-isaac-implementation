4dc6cb7e83cc4bcdf7917654f5f15e13b9ef815c
Isaac Salas <iscprepa@gmail.com>
2025-06-28 14:22:13 -0600

    HitboxComponent added
    HitboxComponent add that monitors collisions with 2 types of objects: Proyectiles and HealthPickups
    - Added script to scene
    - HealthComponent can be assigned
    - Added function that, if there is a HealthComponent assigned, scans when an Area2D collides with it and checks the class of the area
    - After checking the class, it calls a HealthComponent function accordingly

---
+++ scripts/components/hitbox_component.gd
@@ -0,0 +1,13 @@
+extends Area2D
+class_name HitboxComponent
+@export var health_comp : HealthComponent
+@export var can_get_hit : bool
+@export var can_heal : bool
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if health_comp != null:
+		if can_get_hit == true and area is Projectile:
+			health_comp.apply_damage(area.damage)
+		if can_heal == true and area is HealthPickup:
+			health_comp.apply_heal(area.hp)
