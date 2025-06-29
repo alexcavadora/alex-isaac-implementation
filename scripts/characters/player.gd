1192e691504cfa2e3ba50b8e9d7d7cd123c941dc
Isaac Salas <iscprepa@gmail.com>
2025-06-29 14:15:53 -0600

    Player can throw projectiles
    - Fixed projectiles spawning outside main scene
    - Projectiles now get thrown in the direction the player is aiming
    - Found a simpler way to rotate the player sprite when moving
    - Tweaked the Projectile rotation_deg
    - Also, set main_scene.tscn as the main scene in the project

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -7,18 +7,13 @@ var input_direction : Vector2
 # Getting the input inside a 2D Vector and setting the velocity accordingly
 func get_input_movement():
 	input_direction = Input.get_vector("a_press", "d_press", "w_press", "s_press")
+	#print (input_direction)
 	self.velocity = input_direction * speed
-	match input_direction.x:
-		1.0:
-			sprite.rotation_degrees = -90
-		-1.0:
-			sprite.rotation_degrees = 90
-	match input_direction.y:
-		1.0:
-			sprite.rotation_degrees = 0
-		-1.0:
-			sprite.rotation_degrees = 180
-	
+	if input_direction != Vector2(0.0,0.0):
+		sprite.rotation = (global_position.angle_to_point(global_position+input_direction))-1.570796 #-90 grad to rad
+
+
+
 	
 func _physics_process(delta):
 	get_input_movement()
--- scripts/components/throw_component.gd
+++ scripts/components/throw_component.gd
@@ -12,11 +12,13 @@ func get_orientation():
 
 func input_throw():
 	if Input.is_action_just_pressed("throw"):
-		var scene_root = get_tree().root
+		var scene_root = get_tree().root.get_child(0)
 		var instance = PROJECTILE.instantiate()
 		instance.global_position = self.global_position
+		instance.moving_direction = self.global_position-player.global_position
 		scene_root.add_child(instance)
 		
+
 
 func _process(delta):
 	if player != null:
