43ffe065022b3f97d83a3f187d3e26b1486416e4
Isaac Salas <iscprepa@gmail.com>
2025-06-29 12:31:06 -0600

    Added ThrowComponent
    Allows the player to throw projetiles, still needs some work but right now it can:
    - Detect where the player is looking
    - Spawn projectiles
    
    Also, tweaked the player scene to work with the new component.
    
    Fixes needed:
    Projectile still needs to move, right now it just spins in place, and gets spawned outside the main scene.

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 class_name Player
 @export var speed = 300
+@onready var sprite = $Sprite
+var input_direction : Vector2
 
 # Getting the input inside a 2D Vector and setting the velocity accordingly
 func get_input_movement():
-	var input_direction = Input.get_vector("a_press", "d_press", "w_press", "s_press")
+	input_direction = Input.get_vector("a_press", "d_press", "w_press", "s_press")
 	self.velocity = input_direction * speed
+	match input_direction.x:
+		1.0:
+			sprite.rotation_degrees = -90
+		-1.0:
+			sprite.rotation_degrees = 90
+	match input_direction.y:
+		1.0:
+			sprite.rotation_degrees = 0
+		-1.0:
+			sprite.rotation_degrees = 180
 	
-
+	
 func _physics_process(delta):
 	get_input_movement()
 	move_and_slide()
---
+++ scripts/components/throw_component.gd
@@ -0,0 +1,24 @@
+extends Node2D
+class_name ThrowComponent
+
+const PROJECTILE = preload("res://scenes/projectile.tscn")
+@export var player : Player
+@export var offset : int
+var direction : Vector2
+
+func get_orientation():
+	if player.input_direction != Vector2(0.0,0.0):
+		self.position = player.input_direction * offset
+
+func input_throw():
+	if Input.is_action_just_pressed("throw"):
+		var scene_root = get_tree().root
+		var instance = PROJECTILE.instantiate()
+		instance.global_position = self.global_position
+		scene_root.add_child(instance)
+		
+
+func _process(delta):
+	if player != null:
+		get_orientation()
+		input_throw()
