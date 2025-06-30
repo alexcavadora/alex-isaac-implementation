6235cd22bcf920dab35f5d72b472ba4835535637
Isaac Salas <78274906+Isaac-Salas@users.noreply.github.com>
2025-06-29 21:09:05 -0600

    Enviroment tweaks (#21)
    Tweaked Enviroment parameters on the 3D scene to make it look prettier than before, and also added the RTX button to the main scene.
    Also, saved ThrowComponent as its own scene.

--- scripts/scenes/rtx_main_scene.gd
+++ scripts/scenes/rtx_main_scene.gd
@@ -1,10 +1,15 @@
 extends Node3D
+const MAIN_SCENE = preload("res://scenes/levels/main_scene.tscn")
 @onready var directional_light_3d: DirectionalLight3D = $DirectionalLight3D
+@export var x_rot_speed : float = 0.025
+@export var y_rot_speed : float = 0.02
+
 
 func _input(_event):
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 
 func _process(_delta):
-	directional_light_3d.rotation_degrees.x += 0.01
+	directional_light_3d.rotation_degrees.x += x_rot_speed
+	directional_light_3d.rotation_degrees.y += y_rot_speed
 	
